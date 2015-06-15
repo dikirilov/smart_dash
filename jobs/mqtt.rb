08f5599b0730424cb5f1dcbe67f0a876a6a72506
Дмитрий Кириловский <dikirilovskiy@gmai.com>
2015-06-16 01:42:55 +0800

    Just some more moments

--- jobs/mqtt.rb
+++ jobs/mqtt.rb
@@ -10,16 +10,19 @@ MQTT_TOPICS = { 'smart/dash' => 'mqtt_widget',
 # Start a new thread for the MQTT client
 Thread.new {
   MQTT::Client.connect(:host => 'localhost', :port => 1883) do |client|
-    client.subscribe('smart/dash')
+    client.subscribe('smart/#')
  
     # Sets the default values to 0 - used when updating 'last_values'
 #    current_values = Hash.new(0)
  
     client.get do |topic,message|
+p topic
+p message
 #      tag = MQTT_TOPICS[topic]
  #     last_value = current_values[tag]
   #    current_values[tag] = message
-      send_event('mqtt_widget', { text: message })
+      time = Time.now.strftime("%H:%M")
+      send_event('mqtt', { text: message, updatedAt: time, from_info: "MQTT" })
     end
   end
 }
--- jobs/smart.rb
+++ jobs/smart.rb
@@ -12,25 +12,15 @@ SCHEDULER.every '10s' do
   temp_min = forecast.css("forecast day")[1].css("day_part")[1].css("temperature_from").text
   temp_max = forecast.css("forecast day")[1].css("day_part")[1].css("temperature_to").text
 
-#p temp_min
-#p temp_max
+p temp
 
-#  currency = Nokogiri::XML(Net::HTTP.get(URI.parse("http://www.cbr.ru/scripts/XML_daily.asp"))).at_xpath('//ValCurs')
-#  usd = currency.at_xpath('//Valute[@ID="R01235"]/Value').content 
-#  eur = currency.at_xpath('//Valute[@ID="R01239"]/Value').content
-#traff_lev = 6
-#traff_hint = "Движение затруднено"
-  traff_lev = Nokogiri::XML(Net::HTTP.get(URI.parse("http://export.yandex.ru/bar/reginfo.xml?ncrnd=9742"))).at_xpath('//info/traffic/level').content   #").text
+  traff_lev = Nokogiri::XML(Net::HTTP.get(URI.parse("http://export.yandex.ru/bar/reginfo.xml?ncrnd=9742"))).at_xpath('//info/traffic/level').content  
   traff_hint = Nokogiri::XML(Net::HTTP.get(URI.parse("http://export.yandex.ru/bar/reginfo.xml?ncrnd=9742"))).at_xpath('//info/traffic/hint').content 
-#p traffic
-#  traff = traffic.at_xpath('//traffic')
-#  tr_num = traff.at_xpath('//level').content
-#  tr_lev = traff.at_xpath('//hint').content
+
+p traff_lev
+
   time = Time.now.strftime("%H:%M")
 
-#p traff_lev
-
   send_event('forecast', { text: (((temp_min.to_i+temp_max.to_i)/2).round.to_s+"°"), utext: (temp.to_s+"°"), from_info: "Yandex", updatedAt: time, title: "Погода", pre_text: "Сейчас", pre_utext: "Завтра" })
-#  send_event('usd_cur', { text: usd.to_s, from_info: "CBR" })
   send_event('traffic', { text: traff_lev, utext: traff_hint, from_info: "Yandex", updatedAt: time, title: "Пробки" })
 end
